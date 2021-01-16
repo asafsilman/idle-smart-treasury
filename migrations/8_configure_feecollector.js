6e9f4f2163860e636478a39d84ad499675e9fb92
Asaf Silman <asafsilman@gmail.com>
2021-01-17 01:59:59 +0800

    Add rebalancer as beneficiary

--- migrations/8_configure_feecollector.js
+++ migrations/8_configure_feecollector.js
@@ -1,7 +1,14 @@
+const {BN} = require('@openzeppelin/test-helpers')
+
 const FeeCollector = artifacts.require("FeeCollector");
 const SmartTreasuryBootstrap = artifacts.require("SmartTreasuryBootstrap");
 
 const addresses = require('./addresses');
+
+
+const BNify = n => new BN(String(n))
+
+const ratio_one_pecrent = BNify('1000')
 
 module.exports = async function (deployer, network) {
   if (network === 'test' || network === 'development' || network == 'soliditycoverage') {
@@ -17,6 +24,14 @@ module.exports = async function (deployer, network) {
   let crpAddress = await smartTreasuryBootstrapInstance._getCRPAddress.call()
   await feeCollectorInstance.setSmartTreasuryAddress(crpAddress, {from: _addresses.multisig})
   await feeCollectorInstance.addAddressToWhiteList(_addresses.idleRebalancer, {from: _addresses.multisig})
+
+  let allocation = [
+    ratio_one_pecrent.mul(BNify('80')),
+    ratio_one_pecrent.mul(BNify('15')), // fee treasury
+    ratio_one_pecrent.mul(BNify('5'))   // rebalalncer
+  ]
+
+  await feeCollectorInstance.addBeneficiaryAddress(_addresses.idleRebalancer, allocation, {from: _addresses.multisig})
 
   await feeCollectorInstance.replaceAdmin(_addresses.timelock, {from: _addresses.multisig})
 }
