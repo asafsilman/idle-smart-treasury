8178c629908b4bec9525710e13c247834f7dfe23
Asaf Silman <asafsilman@gmail.com>
2021-01-20 12:33:11 +0800

    Use HDWalletProvider by default

--- truffle-config.js
+++ truffle-config.js
@@ -94,8 +94,8 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     kovan: {
-      // provider: () => new HDWalletProvider(mnemonic, `wss://kovan.infura.io/ws/v3/${infuraKey}`),
-      provider: () => new LedgerWalletProvider({...ledgerOptions, networkId: 42}, `https://kovan.infura.io/v3/${infuraKey}`),
+      provider: () => new HDWalletProvider(mnemonic, `wss://kovan.infura.io/ws/v3/${infuraKey}`),
+      // provider: () => new LedgerWalletProvider({...ledgerOptions, networkId: 42}, `https://kovan.infura.io/v3/${infuraKey}`),
       network_id: 42,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
